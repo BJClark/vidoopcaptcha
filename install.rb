4448c1140aea14dec91272c91365ce8b5715ee2f
BJ Clark <bj@aboutus.org>
2009-03-26 10:40:20 -0700

    Setting up install script

--- install.rb
+++ install.rb
@@ -1,1 +1,16 @@
-# Install hook code here
+require 'ftools'
+
+if File.directory? "config"
+  dest_dir = "config"
+  src_config_file = File.join(File.dirname(__FILE__),"vidoop_captcha.yml")
+  dest_config_file = File.join(dest_dir, "vidoop_captcha.yml") if dest_dir
+
+  if File::exists? dest_config_file
+    STDERR.puts "\nA config file already exists at #{dest_config_file}.\n"
+  else
+    File.copy src_config_file, dest_config_file
+  end
+
+else
+  STDERR.puts "\nA config file already exists at #{dest_config_file}.\n"
+end
